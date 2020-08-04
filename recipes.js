8aed14bb45cc121c360b4e4f1a0baf6c1efa4763
Casey D <caseydasilva@hotmail.com>
2020-08-03 18:59:04 -0700

    trying to add HTML elements via JS
    trouble adding the <li>s inside the <ul> elements already added on the page thru JS

--- recipes.js
+++ recipes.js
@@ -1,15 +1,14 @@
-//allReciepes arrStructure[
+//allRecipes arrStructure[
 // [0]name
-// [1]link
-// [2]type
-// [3]ingredients
-// [4]instructions
+// [1]type
+// [2]ingredients
+// [3]instructions
+// [4]link (<a>"original recipe")
 // ]
 
 const allRecipes = [
 	[
 		(name = 'Tartar Sauce'),
-		(link = 'https://natashaskitchen.com/tartar-sauce-recipe/'),
 		(type = [ 'sauce', 'seafood', 'fish' ]),
 		(ingredients = [
 			'1 c mayonnaise',
@@ -19,11 +18,11 @@ const allRecipes = [
 			'1 tsp sugar',
 			'1/4 tsp freshly ground black pepper'
 		]),
-		(instructions = [ 'Combine & Serve!' ])
+		(instructions = [ 'Combine & Serve!' ]),
+		'https://natashaskitchen.com/tartar-sauce-recipe/'
 	],
 	[
 		(name = 'Garlic Lime Seasoning'),
-		(link = 'https://www.garlicandzest.com/tangy-garlic-lime-shrimp-tacos/'),
 		(type = [ 'mexican', 'shrimp tacos', 'seasoning', 'fish', 'seafood' ]),
 		(ingredients = [
 			'1/2 tsp salt',
@@ -40,11 +39,11 @@ const allRecipes = [
 			'Combine all ingredients',
 			'Massage the mixture in a large bowl with the shrimp until well mixed in',
 			'Cook shrimp'
-		])
+		]),
+		'https://www.garlicandzest.com/tangy-garlic-lime-shrimp-tacos/'
 	],
 	[
 		(name = 'Creamy Cilantro Sauce'),
-		(link = 'https://tasty.co/recipe/grilled-shrimp-tacos-with-creamy-cilantro-sauce'),
 		(type = [ 'mexican', 'shrimp tacos', 'sauce', 'fish', 'seafood' ]),
 		(ingredients = [
 			'1 c sour cream',
@@ -55,39 +54,74 @@ const allRecipes = [
 			'3 c mixed cabbage',
 			'1 tbsp very minced jalopeno'
 		]),
-		(instructions = [ 'Combine & Serve!' ])
+		(instructions = [ 'Combine & Serve!' ]),
+		'https://tasty.co/recipe/grilled-shrimp-tacos-with-creamy-cilantro-sauce'
 	]
 ];
 
 const elementBody = document.querySelector('body');
 const scriptTag = document.querySelector('script');
 
-let elementH3;
-
 function addElements() {
 	for (i = 0; i < allRecipes.length; i++) {
-		let elementH3 = document.createElement('h3');
-		elementBody.insertBefore(elementH3, scriptTag);
-		elementH3.innerText = allRecipes[i][0];
-		elementH3.insertAdjacentHTML('afterend', '<ul></ul>');
+		const newH3 = document.createElement('h3');
+		elementBody.insertBefore(newH3, scriptTag);
+		newH3.innerText = allRecipes[i][0];
+		newH3.insertAdjacentHTML('afterend', '<section class="link">Original Recipe</section>');
+		newH3.insertAdjacentHTML('afterend', '<section class="instructions"><ul class="instUl"></ul></section>');
+		newH3.insertAdjacentHTML('afterend', '<section class="ingredients"><ul class="ingUl"></ul></section>');
 	}
+
+	// const instrucSection = document.querySelectorAll('section .instructions');
 }
+
 addElements();
 
-let elementUl;
+const elementsH3 = elementBody.querySelectorAll('h3');
+const ingUl = Array.from(document.querySelectorAll('.ingUl'));
+const instUl = Array.from(document.querySelectorAll('.instUl'));
 
-function addLiElements() {
-	for (let i = 0; i < allRecipes.length; i++) {
-		let ingredients = allRecipes[i][3];
-		let elementUl = document.querySelector('ul');
-		for (j = 0; j < ingredients.length; j++) {
-			elementUl.insertAdjacentHTML('afterbegin', '<li></li>');
-			let elementLi = document.querySelector('li');
-			elementLi.innerText = ingredients[j];
-		}
-	}
+function addIngElements() {
+	allRecipes.forEach((recipe) => {
+		let ingredients = recipe[2];
+		ingredients.forEach((ingredient) => {
+			const newLis = document.createElement('li');
+			newLis.innerHTML = ingredient;
+			newLis.className = 'ingLis';
+			console.log(newLis);
+			// ingUl.forEach((index) => {
+			// 	index.insertAdjacentElement('afterbegin', newLis);
+			// });
+			for (let i = 0; i < newLis.length; i++) {
+				ingUl[i].insertAdjacentElement('afterbegin', newLis);
+			}
+		});
+		// const elementsLi = Array.from(document.querySelectorAll('.ingLis'));
+		// console.log(elementsLi);
+		// ingUl.innerHTML = elementsLi;
+		//console.log(ingUl);
+	});
 }
-addLiElements();
 
-//if i make the elementUl a 'querySelector' i get the text
-//but if it's a 'querySelectorAll' it doesn't work
+// for (let i = 0; i < allRecipes.length; i++) {
+// 	console.log(i);
+// 	let ingredients = allRecipes[i][2];
+// console.log(ingredients);
+// ingredients.forEach((ingredient) => {
+// 	const newLis = document.createElement('li');
+// 	newLis.innerHTML = ingredient;
+// 	console.log(newLis);
+// });
+
+// for (j = 0; j < ingredients.length; j++) {
+// 	console.log(j);
+// 	const newLis = document.createElement('li');
+// 	newLis.innerHTML = ingredients[j];
+// 	elementsUl.forEach((elementUl) => {
+// 		elementUl.insertAdjacentElement('afterbegin', newLis);
+// 	});
+// }
+
+addIngElements();
+
+//
