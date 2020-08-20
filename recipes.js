baec329dbcd175c1a743103d773876a823e43a19
Casey D <caseydasilva@hotmail.com>
2020-08-20 11:24:50 -0700

    refactoring

--- recipes.js
+++ recipes.js
@@ -25,23 +25,9 @@ function addElements() {
 		const fullRecipeContainer = document.createElement('div'); //contains all recipe info & the title
 		const recipeTitle = document.createElement('h3');
 		const recipeBody = document.createElement('div'); // contains all recipe info NOT the title
-		elementBody.append(fullRecipeContainer);
-		fullRecipeContainer.append(recipeBody);
-		fullRecipeContainer.prepend(recipeTitle);
-		recipeBody.setAttribute('id', `${recipe.recipeBodyId}`);
-		recipeBody.className = 'hidden';
-		fullRecipeContainer.className = 'col-sm-5';
-		recipeTitle.innerText = recipe.name;
-		recipeTitle.className = 'title';
-		recipeBody.insertAdjacentHTML('afterbegin', `<section id="${recipe.linkId}"></section>`);
-		recipeBody.insertAdjacentHTML(
-			'afterbegin',
-			`<h4 class="subtitle">Instructions</h4><section class="instructions"><ul id="${recipe.instructionsId}"></ul></section>`
-		);
-		recipeBody.insertAdjacentHTML(
-			'afterbegin',
-			`<h4 class="subtitle">Ingredients</h4><section class="ingredients"><ul id="${recipe.ingredientsId}"></ul></section>`
-		);
+		addRecipeBody(recipeBody, recipe);
+		addRecipeContainer(fullRecipeContainer, recipeBody, recipeTitle);
+		addRecipeTitle(recipeTitle, recipe);
 
 		const ingredients = recipe.ingredients;
 		const instructions = recipe.instructions;
@@ -52,17 +38,38 @@ function addElements() {
 		addItems(ingredients, ingredientsList, 'ingredient');
 		addItems(instructions, instructionsList, 'instruction');
 		addLinks(links, linkAnchor);
+		addClick(recipeTitle, recipeBody, fullRecipeContainer);
 
 		rowContainer.append(fullRecipeContainer); // adding content into parent 'row' div
-
-		//adding Event Listener to toggle recipes upon click of title
-		recipeTitle.addEventListener('click', function() {
-			recipeBody.classList.toggle('appear');
-			recipeBody.classList.remove('col-sm-5');
-			fullRecipeContainer.classList.toggle('col-sm-12');
-		});
 	});
 }
+
+function addRecipeContainer(fullRecipeContainer, recipeBody, recipeTitle) {
+	elementBody.append(fullRecipeContainer);
+	fullRecipeContainer.append(recipeBody);
+	fullRecipeContainer.prepend(recipeTitle);
+	fullRecipeContainer.className = 'col-sm-5';
+}
+
+function addRecipeTitle(recipeTitle, recipe) {
+	recipeTitle.innerText = recipe.name;
+	recipeTitle.className = 'title';
+}
+
+function addRecipeBody(recipeBody, recipe) {
+	recipeBody.setAttribute('id', `${recipe.recipeBodyId}`);
+	recipeBody.className = 'hidden';
+	recipeBody.insertAdjacentHTML('afterbegin', `<section id="${recipe.linkId}"></section>`);
+	recipeBody.insertAdjacentHTML(
+		'afterbegin',
+		`<h4 class="subtitle">Instructions</h4><section class="instructions"><ul id="${recipe.instructionsId}"></ul></section>`
+	);
+	recipeBody.insertAdjacentHTML(
+		'afterbegin',
+		`<h4 class="subtitle">Ingredients</h4><section class="ingredients"><ul id="${recipe.ingredientsId}"></ul></section>`
+	);
+}
+
 //adding ingredients & instructions lists
 function addItems(items, itemsList, addClass) {
 	items.forEach((item) => {
@@ -81,7 +88,31 @@ function addLinks(links, linkAnchor) {
 	linkAnchor.append(newAnchor);
 }
 
+//adding click func to recipe titles
+function addClick(recipeTitle, recipeBody, fullRecipeContainer) {
+	recipeTitle.addEventListener('click', function() {
+		recipeBody.classList.toggle('appear');
+		recipeBody.classList.remove('col-sm-5');
+		fullRecipeContainer.classList.toggle('col-sm-12');
+	});
+}
+
 addElements();
+
+// add search element (in progress...)
+
+const recipes = [];
+allRecipes.forEach((recipe) => {
+	return recipes.push(recipe);
+});
+console.log(recipes);
+
+function findMatches(wordToMatch, recipes) {
+	return recipes.filter((item) => {
+		const regex = RegExp(wordToMatch, 'gi');
+		return item.recipe.match(regex);
+	});
+}
 
 //add animation
 // let start = Date.now();
